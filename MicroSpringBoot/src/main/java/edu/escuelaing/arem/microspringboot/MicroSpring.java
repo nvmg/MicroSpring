c4d235f7e0a74f8abbe8afd65af6995ed73b663b
unknown <nicole.montana@labinfo.is.escuelaing.edu.co>
2022-09-07 19:18:00 -0500

    Arreglo 3

--- MicroSpringBoot/src/main/java/edu/escuelaing/arem/microspringboot/MicroSpring.java
+++ MicroSpringBoot/src/main/java/edu/escuelaing/arem/microspringboot/MicroSpring.java
@@ -39,13 +39,12 @@ public class MicroSpring {
             }
         }
         ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(35000);
+         try {
+            serverSocket = new ServerSocket(getPort());
         } catch (IOException e) {
-            System.err.println("Could not listen on port: 35000.");
+            System.err.println("Could not listen on port: "+getPort());
             System.exit(1);
         }
-
         boolean run = true;
         while (run) {
             Socket clientSocket = null;
@@ -93,6 +92,13 @@ public class MicroSpring {
         serverSocket.close();
     }
     }
+    
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+        return Integer.parseInt(System.getenv("PORT"));
+    }
+    return 4567;
+ }
    
     
     }
